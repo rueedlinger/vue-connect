4e0cb362f8e420cd95e7d15da02e41df80ae311e
Matthias Rüedlinger <matthias.ruedlinger@spoud.io>
2021-02-04 22:51:59 +0100

    minor fixes docker and ci build

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 if [ -z "$@" ]; then
     echo "Starting vue-connect services"
     echo "VC_VERSION: $VC_VERSION"
+    echo "VC_TAGS: $VC_TAGS"
     echo "VC_LABELS: $VC_LABELS"
     echo "VC_DIGEST: $VC_DIGEST"
     exec /usr/bin/supervisord -c /etc/supervisord.conf
