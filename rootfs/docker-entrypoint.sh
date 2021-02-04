39cd329f80609873a8ba66ac1353477922ae2aca
Matthias Rüedlinger <matthias.ruedlinger@spoud.io>
2021-02-04 10:51:27 +0100

    added docker args and envs

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 if [ -z "$@" ]; then
     echo "Starting vue-connect services"
+    echo "VC_VERSION: $VC_VERSION"
+    echo "VC_LABELS: $VC_LABELS"
+    echo "VC_DIGEST: $VC_DIGEST"
     exec /usr/bin/supervisord -c /etc/supervisord.conf
 else
     exec $@
