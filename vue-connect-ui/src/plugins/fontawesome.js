bf1a09094b20b14dc2ee8bae0cb89c7bac56709d
Matthias Rüedlinger <matthias.ruedlinger@spoud.io>
2021-02-08 23:06:48 +0100

    fix: only import used fontawesome icons  #30

--- vue-connect-ui/src/plugins/fontawesome.js
+++ vue-connect-ui/src/plugins/fontawesome.js
@@ -1,9 +1,37 @@
 import Vue from "vue";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-library.add(fas);
+// only import the icons which are used
+import {
+  faCog,
+  faInfoCircle,
+  faEdit,
+  faTrashAlt,
+  faRetweet,
+  faPlayCircle,
+  faPauseCircle,
+  faSyncAlt,
+  faPlusCircle,
+  faSave,
+  faCogs,
+  faLayerGroup,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faCog,
+  faInfoCircle,
+  faEdit,
+  faTrashAlt,
+  faRetweet,
+  faPlayCircle,
+  faPauseCircle,
+  faSyncAlt,
+  faPlusCircle,
+  faSave,
+  faCogs,
+  faLayerGroup
+);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
