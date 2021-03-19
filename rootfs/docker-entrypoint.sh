ed5b8c0f049a34ec4a0fe98d75aaf5597cccbc85
Matthias Rueedlinger <md@rueedlinger.ch>
2021-03-19 14:59:49 +0100

    feat: added redis

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -3,19 +3,6 @@ set -e
 
 echo "TZ: $TZ"
 echo "LANG: $LANG"
-echo "VC_SQLITE_FILE_PATH: $VC_SQLITE_FILE_PATH"
-
-# SQL script to create db
-SQL_SCRIPT="/dist/python/schema.sql"
-
-# create db
-if [ -f "$VC_SQLITE_FILE_PATH" ]; then
-    echo "$VC_SQLITE_FILE_PATH exists already! The SQL script $SQL_SCRIPT is not executed."
-else 
-    echo "$VC_SQLITE_FILE_PATH does not exist. Running SQL script $SQL_SCRIPT."
-    sqlite3 $VC_SQLITE_FILE_PATH < /dist/python/schema.sql 
-    chown gunicorn:gunicorn $VC_SQLITE_FILE_PATH
-fi
 
 echo "Starting vue-connect"
 echo "VC_VERSION: $VC_VERSION"
